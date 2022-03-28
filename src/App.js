bbb1307ba689dc9aa640eacd46f8a53b03ffe93d
devnerds <89212105+DevNerds2020@users.noreply.github.com>
2022-03-28 21:45:17 +0430

    adding firestore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 import { signInWithGoogle } from './service/firebase'
 import { useState } from 'react'
 import { useEffect } from 'react';
 import { auth } from './service/firebase'
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth' ;
+import 'firebase/compat/firestore'
 import { gitProvider } from './service/firebase';
 function App() {
   const [user, setUser] = useState(null);
@@ -15,14 +15,32 @@ function App() {
       setUser(user)
     })
   }, [])
+  const googleSignInHandler = () => {
+    signInWithGoogle().then(result => {
+      console.log(result.user.displayName)
+      const users = firebase.firestore().collection('users')
+      console.log(users)
+      users.doc(result.user.uid).get().then( (doc) => {
+        if (!doc.exists){
+          users.add({
+            users: result.user.displayName
+          })
+        }
+      })
+    })
+  }
   return (
-    <div className="App">
+    <div className="App" style={{display:"flex", alignItems:'center', justifyContent: 'center', flex: 1}}>
       
       {user===null 
       ? 
-      <div>
-        <button className="button" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
-        <button className="button" onClick={() => {auth.signInWithPopup(gitProvider)}}><i className="fab fa-google"></i>Sign in with github</button>
+      <div style={{display:'flex', justifyContent: 'space-around', flexDirection:'column'}}>
+        <div>
+          <button className="button" onClick={googleSignInHandler}><i className="fab fa-google"></i>Sign in with google</button>
+        </div>
+        <div>
+          <button className="button" onClick={() => {auth.signInWithPopup(gitProvider)}}><i className="fab fa-google"></i>Sign in with github</button>
+        </div>  
       </div> 
       :
         <div>
